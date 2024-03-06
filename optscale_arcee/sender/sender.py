71dbb0975e687f5c0d68cfd05f7048944a031868
nk-hystax <nkovaleva@hystax.com>
2024-03-06 11:12:45 +0300

    OS-7355. Changed model/application to task

--- optscale_arcee/sender/sender.py
+++ optscale_arcee/sender/sender.py
@@ -78,8 +78,8 @@ class Sender:
                 return await response.json()
 
     @check_shutdown_flag_set
-    async def get_run_id(self, model_key, token, run_name):
-        uri = "%s/applications/%s/run" % (self.endpoint_url, model_key)
+    async def get_run_id(self, task_key, token, run_name):
+        uri = "%s/tasks/%s/run" % (self.endpoint_url, task_key)
         headers = {"x-api-key": token, "Content-Type": "application/json"}
         data = {
             "imports": await self._imports_data(),
@@ -136,7 +136,7 @@ class Sender:
         return await self.send_post_request(uri, headers, data)
 
     @check_shutdown_flag_set
-    async def register_dataset(self, run_id, run_name, model_key, path, token):
+    async def register_dataset(self, run_id, run_name, task_key, path, token):
         uri = f"{self.endpoint_url}/run/{run_id}/dataset_register"
         headers = {"x-api-key": token, "Content-Type": "application/json"}
 
@@ -144,7 +144,7 @@ class Sender:
             "path": path,
             "name": f"Dataset {int(datetime.utcnow().timestamp())}",
             "description": f"Discovered in training "
-                           f"{model_key} - {run_name}({run_id})"
+                           f"{task_key} - {run_name}({run_id})"
         }
         await self.send_post_request(uri, headers, data)
 
